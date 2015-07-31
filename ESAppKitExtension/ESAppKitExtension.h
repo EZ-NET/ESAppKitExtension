d3f3ff8ab09a2c217c9b61d8dc4502522fe7d79c
Tomohiro Kumagai <tomohiro@ez-style.jp>
2015-07-31 23:02:30 +0900

    Initial Commit

---
+++ ESAppKitExtension/ESAppKitExtension.h
@@ -0,0 +1,19 @@
+//
+//  ESAppKitExtension.h
+//  ESAppKitExtension
+//
+//  Created by Tomohiro Kumagai on H27/07/31.
+//  Copyright © 平成27年 EasyStyle G.K. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ESAppKitExtension.
+FOUNDATION_EXPORT double ESAppKitExtensionVersionNumber;
+
+//! Project version string for ESAppKitExtension.
+FOUNDATION_EXPORT const unsigned char ESAppKitExtensionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ESAppKitExtension/PublicHeader.h>
+
+
